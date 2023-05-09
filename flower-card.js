17baf80801922fe90507f6aeb3a7b5b8078ca97d
Ardelean Călin <artech@duck.com>
2023-05-09 17:07:12 +0300

    Rounded decimal.

--- flower-card.js
+++ flower-card.js
@@ -189,7 +189,7 @@ customElements.whenDefined("card-tools").then(() => {
           if (result[elem]) {
             limits["max_" + elem] = result[elem].max;
             limits["min_" + elem] = result[elem].min;
-            curr[elem] = result[elem].current;
+            curr[elem] = result[elem].current.toFixed(1);
             icons[elem] = result[elem].icon;
             sensors[elem] = result[elem].sensor;
             uomt[elem] = result[elem].unit_of_measurement;
